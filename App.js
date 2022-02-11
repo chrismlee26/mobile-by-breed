e4ee0c70a06fdc93cc4521a2d7647169381f2654
CHRIS_LEE <chrismlee87@gmail.com>
2022-02-11 00:05:55 -0800

    Add keyboardAvoidingView for iOS & Android

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, FlatList, SafeAreaView, TextInput } from 'react-native';
+import { StyleSheet, Text, View, FlatList, SafeAreaView, TextInput, KeyboardAvoidingView, Platform } from 'react-native';
 
 import Item from './Item'
 
@@ -11,30 +11,35 @@ export default function App() {
 
   return (
     <SafeAreaView style={styles.container}>
-      <StatusBar style="light" />
-      <View style={styles.itemContainer}>
-        <Text style={styles.title}>MittensList 🐈</Text>
-        <TextInput
-          style={styles.searchBar}
-          placeholder="Search"
-          onChangeText={setSearch}
-          value={search}
-          placeholderTextColor={'white'}
-          opacity={0.6}
-        />
-        <FlatList
-          data={cats.filter(item => item.breed.includes(search))}
-          ItemSeparatorComponent={(props) => {
-            return (
-              <View style={styles.separator} />
-            );
-          }}
-          keyExtractor={item => `${item.index}${item.breed}`}
-          renderItem={({ item, index }) => {
-            return <Item key={index} index={index} data={item} />
-          }}
-        />
-      </View>
+      <KeyboardAvoidingView
+        behavior={Platform.OS === "ios" ? "padding" : "height"}
+        style={styles.kav}
+      >
+        <StatusBar style="light" />
+        <View style={styles.itemContainer}>
+          <Text style={styles.title}>MittensList 🐈</Text>
+          <TextInput
+            style={styles.searchBar}
+            placeholder="Search"
+            onChangeText={setSearch}
+            value={search}
+            placeholderTextColor={'white'}
+            opacity={0.6}
+          />
+          <FlatList
+            data={cats.filter(item => item.breed.includes(search))}
+            ItemSeparatorComponent={(props) => {
+              return (
+                <View style={styles.separator} />
+              );
+            }}
+            keyExtractor={item => `${item.index}${item.breed}`}
+            renderItem={({ item, index }) => {
+              return <Item key={index} index={index} data={item} />
+            }}
+          />
+        </View>
+      </KeyboardAvoidingView>
     </SafeAreaView>
   );
 }
@@ -70,5 +75,10 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
     textAlign: 'center',
-  }
+  },
+  kav: {
+    flex: 1,
+    justifyContent: 'center',
+    width: '100%',
+  },
 });
